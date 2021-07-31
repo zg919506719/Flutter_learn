62886691a753351770d28d1371350e6aa4667fc1
xiaogang <zg919506719@163.com>
2021-07-31 13:31:39 +0800

    learn tab

--- flutter_learn/lib/my_tab.dart
+++ flutter_learn/lib/my_tab.dart
@@ -7,41 +7,38 @@ class MyTab extends StatelessWidget {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-          body: Container(
-            child:  Center(
-              child: Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: TabBar(
-                        onTap: (int i){
-
-                        },
-                        tabs: [
-                          Tab(
-                            child: Text("tab1"),
-                          ),
-                          Tab(
-                            child: Text("tab2"),
-                          )
-                        ]),
+            body: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: TabBar(
+                      labelColor: Colors.black12,
+                      unselectedLabelColor: Colors.red,
+                      indicatorColor: Colors.green,
+                      indicatorWeight: 5,
+                      onTap: (int i) {},
+                      tabs: [
+                        Tab(
+                          child: Text("tab1"),
+                        ),
+                        Tab(
+                          child: Text("tab2"),
+                        )
+                      ]),
+                ),
+                Expanded(
+                    child: TabBarView(children: [
+                  Center(
+                    child: Text("123"),
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child:                   TabBarView(children: [
-                      Center(
-                        child: Text("123"),
-                      ),
-                      Text("321")
-                    ])
-                  )
-                ],
-              ),
+                  Text("321")
+                ]))
+              ],
             ),
-          )
-
-        ));
+          ),
+        )));
   }
 }
